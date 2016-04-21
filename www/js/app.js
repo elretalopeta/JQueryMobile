949fac122cb41aaf8f1c55f4e53193d5bb3dc4b4
eric retamero <eric2reta@gmail.com>
2016-04-21 20:09:55 +0200

    detalle

--- www/js/app.js
+++ www/js/app.js
@@ -2,27 +2,22 @@ function onAppReady() {
     if( navigator.splashscreen && navigator.splashscreen.hide ) {   // Cordova API detected
         navigator.splashscreen.hide() ;
     }
-    $('#login').click(home);
-    
-    //$('#demo').click(demo);
 
+    $('#login').click(home);
     $('#home-load').click(function(){
         load(1);
     });
     $('#detalle-load').click(function(){
         load(2);
     });
-
-    $('#add').click(contenido);
-    $('#añadir').click(contenido);
-
     $('#guardar').click(guardar);
-    $('.ir').click(detalle);
+    $('#return').click(vaciar);
+    $('.vista-detalle').click(detalle);
     
 }
 
 function demo(){
-    alert('a');
+   
 };
 
 var imagen;
@@ -40,7 +35,6 @@ switch(type){
 
     break;
     case 2:
-    //alert('Tipo 2');
     navigator.camera.getPicture(detallePictureSuccess, onFail, {
          quality: 100
     , saveToPhotoAlbum: true
@@ -53,12 +47,17 @@ switch(type){
    }
 };
 
-var id = 1;
+var id = 0; //key para saber imagen slider (0-2)
 function detallePictureSuccess(imageData) { 
-    if(id>3)id=1;
-    var imagen = $('#detalle-img-'+id);
-    imagen.attr('src', "data:image/jpeg;base64," + imageData);
+    
+    if(id>2)id = 0;
+    var imagenDetalle = $('#img-detalle-'+id);
+    imagenDetalle.attr('src', "data:image/jpeg;base64," + imageData);
+    var objetoRemplazar = JSON.parse(localStorage.getItem(idItem));
+    objetoRemplazar[4][id] = imagenDetalle.attr('src');
+    localStorage.setItem("item"+key, JSON.stringify(objetoRemplazar));
     id++;
+    alert(id);
 }
 
 var imagen;
@@ -67,43 +66,74 @@ function homePictureSuccess(imageData) {
     imagen.attr('src', "data:image/jpeg;base64," + imageData);
 }
 
+var key = 1;
 function guardar(){
-    //var elemento = $('<li class="ui-li-has-thumb ui-last-child"><a href="#detalle" class="ui-btn ui-btn-icon-right ui-icon-carat-r"><img src="'+ imagen.attr('src')+'" /><h2>Broken Bells</h2><p>Broken Bells</p></a></a></li>');
-    //var objeto = imagen;
-    //var elemento = $('<li class="ui-li-has-thumb ui-last-child"><a href="#" class="ui-btn ui-btn-icon-right ui-icon-carat-r"> <h2>Broken Bells</h2><p>Broken Bells</p></a></li>');
-    //elemento.$("a").after(imagen);
+
+    if(localStorage.getItem("key") == null){
+        localStorage.setItem("key", 1);
+    }else{  
+      key++;
+        localStorage.setItem("key", key);
+    }
 
     var nombre = $('#nombre').val();
     var anchura = $('#anchura').val();
     var altura = $('#altura').val();
     var profundida = $('#profundidad').val();
-    var objeto = [nombre, anchura, altura, profundida];
+    var imagenes = [imagen.attr('src')];
+    var objeto = [nombre, anchura, altura, profundida, imagenes];
+   
 
-    //JSON.stringify
+    var elemento = $('<li class="ui-li-has-thumb ui-last-child vista-detalle" id="item'+key+'"><a href="#detalle" class="ui-btn ui-btn-icon-right ui-icon-carat-r identificador"><img src="'+imagen.attr('src')+'"/><h2>'+nombre+'</h2></a></li>');
+    localStorage.setItem("item"+key, JSON.stringify(objeto));
+    var id = $('#ubicacion').val(); //seleccionar el campo tipo comedor/cocina/dormitorio
+    $("#"+id).after(elemento); //añado el elemento a la lista
+    $('.vista-detalle').click(detalle); //remarco para que me haga esta funcion en los elementos creados
+    
+    $('#nombre').val("");
+    $('#anchura').val("0");
+    $('#altura').val("0");
+    $('#profundidad').val("0");
+    $('#img').attr("src", "img/default.png");
 
-    localStorage.setItem("objeto", JSON.stringify(objeto));
 
-    var id = $('#ubicacion').val();
-    //$("#"+id).after(elemento);
+    //$('#ver').html("Resultado: "+objeto[4][0]);
 };
-
-function contenido(){
-    //var imagen = $('<li class="ui-li-has-thumb ui-last-child"><a href="#" class="ui-btn ui-btn-icon-right ui-icon-carat-r"><img src="img/cocina.jpg"><h2>Broken Bells</h2> <p>Broken Bells</p></a></a></li>');
-    //$('#comedor').after(imagen);
-
-    var anchura = $('#anchura').val();
-    $('#ver').html(anchura);
+function vaciar(){
+    $("#img-detalle-2").attr("src", "img/default.png");
+    $("#img-detalle-3").attr("src", "img/default.png");
 }
 
+var idItem = 0;
 function detalle(){
-    var objeto = JSON.parse(localStorage.getItem("objeto"));
-    //var objeto = localStorage.getItem("objeto");
-    //document.getElementById("ver2").innerHTML = objeto[0];
-    //document.getElementById("ver2").append = objeto[0];
-    $("#nombre-detalle").append(objeto[0]);
-    $("#anchura-detalle").append(objeto[1]);
-    $("#altura-detalle").append(objeto[2]);
-    $("#profundidad-detalle").append(objeto[3]);
+    idItem = $(this).attr('id');
+    var objeto = JSON.parse(localStorage.getItem(idItem));
+    $("#nombre-detalle").html("Nombre: "+objeto[0]);
+    $("#anchura-detalle").html("Anchura: "+objeto[1]);
+    $("#altura-detalle").html("Altura: "+objeto[2]);
+    $("#profundidad-detalle").html("Profundidad: "+objeto[3]);
+    cargarImagenes(objeto);
+    id = objeto[4].length;
+}
+
+function cargarImagenes(objeto){
+
+    switch(objeto[4].length){
+        case 1:
+          $("#img-detalle-0").attr("src", objeto[4][0]);
+        break;
+
+        case 2:
+            $("#img-detalle-0").attr("src", objeto[4][0]);
+            $("#img-detalle-1").attr("src", objeto[4][1]);
+        break;
+        case 3:
+            $("#img-detalle-0").attr("src", objeto[4][0]);
+            $("#img-detalle-1").attr("src", objeto[4][1]);
+            $("#img-detalle-2").attr("src", objeto[4][2]);
+        break;
+    }
+
 }
 
 function home(){
